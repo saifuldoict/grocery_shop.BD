dbcdbd2d6a2b33a33c312e149a6ed6764f6546cf
Md. Saiful Islam <saiful.doict@gmail.com>
2025-05-06 16:55:06 +0600

    search bar updated

--- frontend/src/components/BestSellers.jsx
+++ frontend/src/components/BestSellers.jsx
@@ -8,7 +8,7 @@ const BestSellers = () => {
     <div className='mt-16 '>
         <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
         
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md: gap-6 lg:grid-cols-5  mt-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-2 sm:gap-2 ml-3 pb-2 md:grid-cols-3 gap-4 lg:grid-cols-6  mt-6'>
           {products.filter((product)=>product.inStock).slice(0,5).map((product,index)=>(
             <ProductCard key={(index)} product={product}/>
           ))}
