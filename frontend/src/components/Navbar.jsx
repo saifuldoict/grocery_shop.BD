4b1fffe5586e03a8bc2fa8e211fdbc501fe83649
Md. Saiful Islam <saiful.doict@gmail.com>
2025-05-06 15:00:08 +0600

    navbar Search product run

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
@@ -6,12 +6,18 @@ import { IoSearchSharp } from "react-icons/io5";
 const Navbar = () => {
     
     const [open, setOpen] = useState(false)
-    const {user, setUser,  setShowUserLogin, navigate} = useAppContext()
+    const {user, setUser,  setShowUserLogin, navigate,searchQuery, setSearchQuery} = useAppContext()
 
  const logout = async()=>{
     setUser(null);
     navigate('/')
  }
+
+ useEffect(()=>{
+    if(searchQuery.length >0){
+        navigate('/products')
+    }
+ },[searchQuery])
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
@@ -32,7 +38,7 @@ const Navbar = () => {
                 
               
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e)=>setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <IoSearchSharp className='text-2xl'/>
                     
                 </div>
