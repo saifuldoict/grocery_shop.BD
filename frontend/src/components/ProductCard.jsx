7f14e3972f3db7292c26eb39460b8b0591ac864f
Md. Saiful Islam <saiful.doict@gmail.com>
2025-05-07 16:53:33 +0600

    productDetails page added

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { assets } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
 
--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,27 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppContext } from '../context/AppContext';
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
-    const product = {
-        name: "Casual Shoes",
-        category: "Sports",
-        price: 100,
-        offerPrice: 80,
-        rating: 4,
-        images: [
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage.png",
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage2.png",
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage3.png",
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage4.png"
-        ],
-        description: [
-            "High-quality material",
-            "Comfortable for everyday use",
-            "Available in different sizes"
-        ]
-    };
+    const {products, navigate, currency, addToCart} = useAppContext()
+    const {id} = useParams()
+    const [relatedProducts, setRelatedProducts] = useState(product.images[0]);
+    const [thumbnail, setThumbnail] = useState(null);
 
-    const [thumbnail, setThumbnail] = React.useState(product.images[0]);
-  return (
+const product = products.find((item)=>item._id===id);
+
+
+  return prduct &&(
     <div className="max-w-6xl w-full px-6">
             <p>
                 <span>Home</span> /
