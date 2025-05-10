bb4aec99d006eb7bf862bf7db07940e646d8de5f
Md. Saiful Islam <saiful.doict@gmail.com>
2025-05-10 12:56:08 +0600

    Add Address page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import AllProducts from './pages/AllProducts'
 import ProductCategroy from './pages/ProductCategroy'
 import ProductDetails from './pages/ProductDetails'
 import CartPage from './pages/CartPage'
+import AddAddress from './pages/AddAddress'
 function App() {
   const {showUserLogin} = useAppContext()
 const isSellerPath = useLocation().pathname.includes("seller")
@@ -34,6 +35,7 @@ const isSellerPath = useLocation().pathname.includes("seller")
           <Route path='/products/:category' element={<ProductCategroy/>}/>
           <Route path='/products/:category/:id' element={<ProductDetails/>}/>
           <Route path='/cart' element={<CartPage/>}/>
+          <Route path='/add-address' element={<AddAddress/>}/>
         </Routes>
         </div>
         <div className='mt-5'>
