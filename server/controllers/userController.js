5166a10387f94e56f6ca2d084bdc1a134174f8b7
Md. Saiful Islam <saiful.doict@gmail.com>
2025-05-21 16:17:35 +0600

    user route.js added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ import UserModel from "../models/user";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 
-//Register Use
+//Register Use : /api/user/register
 export const register = async (req, res)=>{
     try{
         const {name, email, password}= req.body;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db.js';
 import 'dotenv/config'
+import userRouter from './routes/userRoute.js';
 const app = express();
 
 const port = process.env.PORT || 4000;
@@ -19,6 +20,8 @@ app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}))
 
 app.get('/', (req, res)=> res.send("API is Working"))
+
+app.use('/api/user', userRouter)
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
